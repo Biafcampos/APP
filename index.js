6a59e90943a27bf0d42cf093be6b50d6cc7ae790
Bianca <66906291+Biafcampos@users.noreply.github.com>
2024-09-10 18:23:15 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,10 @@
+// hello world
+const mensagem = "hello world!"
+
+{
+  const  mensagem = "Hello me"
+  console.log(mensagem)
+}
+
+console.log(mensagem);
+//30:51 minutos em que eu parei da aula 1#
